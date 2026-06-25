3e744cbeed3260855d0a19de60aaaed631ea5360
Netsi12 <netsiabate12@gmail.com>
2026-06-25 13:46:54 +0300

    git commit -m

--- obj/Debug/net10.0/TmsApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/TmsApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TmsApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0b8b8cc482eb5769b48a85dbee6019fcaa7fe21")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8094146a9887b1513b7c8c7393e7a3e47d79c05c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TmsApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TmsApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
